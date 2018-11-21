8a733c57337d8ed4538f758bf47fc5548a9785b9
Ramya CANABADY <jeyacumar.ramya@gmail.com>
2018-11-21 17:28:58 +0100

    searchPromotionAndCategory dto

---
+++ src/main/java/fr/afcepf/al32/wsrecherche/dto/SearchPromotionAndCategoryDto.java
@@ -0,0 +1,19 @@
+package fr.afcepf.al32.wsrecherche.dto;
+
+import java.util.List;
+
+import fr.afcepf.al32.wsrecherche.entity.CategoryProduct;
+import fr.afcepf.al32.wsrecherche.entity.Promotion;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+public class SearchPromotionAndCategoryDto {
+
+	private List<Promotion> promotions ;
+
+	private CategoryProduct selectedCategory;
+
+	private List<String> keyWords ;
+}
